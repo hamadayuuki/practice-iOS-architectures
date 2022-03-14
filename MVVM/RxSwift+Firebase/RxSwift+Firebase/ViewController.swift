49e6dc2fe7e59120a6c076794bb17141bb1e724f
H-Yuuki <yuukirinnma@gmail.com>
2022-03-14 16:05:55 +0900

    add: MVVM/RxSwift+Firebase の初期ファイル作成, .gitignoreの修正

---
+++ MVVM/RxSwift+Firebase/RxSwift+Firebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwift+Firebase
+//
+//  Created by 濵田　悠樹 on 2022/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
