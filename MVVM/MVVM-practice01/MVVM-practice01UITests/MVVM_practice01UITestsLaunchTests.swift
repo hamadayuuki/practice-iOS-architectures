c0c1397c7f840c6e662f7823462f033101620ffa
H-Yuuki <yuukirinnma@gmail.com>
2022-07-25 18:22:21 +0900

    add: StoryBoardなしの環境構築, ライブラリ(RxSwift, SnapKit)のインストール

---
+++ MVVM/MVVM-practice01/MVVM-practice01UITests/MVVM_practice01UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MVVM_practice01UITestsLaunchTests.swift
+//  MVVM-practice01UITests
+//
+//  Created by 濵田　悠樹 on 2022/07/25.
+//
+
+import XCTest
+
+class MVVM_practice01UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
