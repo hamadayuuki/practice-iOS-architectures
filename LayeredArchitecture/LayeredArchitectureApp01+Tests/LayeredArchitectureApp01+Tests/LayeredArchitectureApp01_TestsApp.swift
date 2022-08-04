be45553eaec7cfe18e0c366818c4c1d8a5df957c
H-Yuuki <yuukirinnma@gmail.com>
2022-08-04 22:42:13 +0900

    add: 初期ファイル作成

---
+++ LayeredArchitecture/LayeredArchitectureApp01+Tests/LayeredArchitectureApp01+Tests/LayeredArchitectureApp01_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayeredArchitectureApp01_TestsApp.swift
+//  LayeredArchitectureApp01+Tests
+//
+//  Created by 濵田　悠樹 on 2022/08/04.
+//
+
+import SwiftUI
+
+@main
+struct LayeredArchitectureApp01_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
