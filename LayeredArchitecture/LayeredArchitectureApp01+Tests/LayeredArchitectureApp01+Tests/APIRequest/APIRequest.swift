901b239be8741a0fb1c63744d949119b554ea812
H-Yuuki <yuukirinnma@gmail.com>
2022-08-06 02:55:49 +0900

    update: async/await を使ったAPI通信を実装

--- LayeredArchitecture/LayeredArchitectureApp01+Tests/LayeredArchitectureApp01+Tests/APIRequest/APIRequest.swift
+++ LayeredArchitecture/LayeredArchitectureApp01+Tests/LayeredArchitectureApp01+Tests/APIRequest/APIRequest.swift
@@ -10,28 +10,35 @@ import Foundation
 
 class APIRequest {
     @Published var items = [Decodable]()   // ここを外部から参照する
-    private let qiitaURL = "https//qiita.com/api/v2"
+    private let qiitaURL = "https://qiita.com/api/v2"
     
     //         ↓ APIから受け取るデータの型を可変にしている, RequestComponentを挟んで ~~RequestComponent を呼び出す
-    public func fetch<T: RequestComponent>(component: T) {
+    public func fetch<T: RequestComponent>(component: T) async throws -> [T.type] {
         let url = URL(string: "\(qiitaURL)/items?query=\(component.query)&page=1&per_page=50")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let decoder = JSONDecoder()
+        let items = try decoder.decode([T.type].self, from: data)
+        print(items)
+        return items
         
-        URLSession.shared.dataTask(with: url) { (data, response, error) in
-            do {
-                if let data = data {
-                    let decoder = JSONDecoder()
-                    decoder.keyDecodingStrategy = .convertFromSnakeCase
-                    //                             ↓ なぜ component ではなく T なのか
-                    let items = try decoder.decode(T.type.self, from: data)
-                    DispatchQueue.main.async {
-                        self.items = items as! [Decodable]
-                        print(self.items)
-                    }
-                }
-            } catch {
-                print("APIからのデータ取得でエラー発生")
-            }
-        }
-        .resume()
+//        URLSession.shared.dataTask(with: url) { (data, response, error) in
+//            do {
+//                if let data = data {
+//                    let decoder = JSONDecoder()
+//                    //                             ↓ なぜ component ではなく T なのか
+//                    let items = try decoder.decode([T.type].self, from: data)
+//                    DispatchQueue.main.async {
+//                        self.items = items
+//                        print(self.items)
+//                    }
+//                }
+//            } catch {
+//                print("APIからのデータ取得でエラー発生")
+//                print(error)
+//            }
+//        }
+//        .resume()
     }
 }
