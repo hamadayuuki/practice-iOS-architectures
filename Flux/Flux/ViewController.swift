d6a89176098244e1da8fde9e06e051501cda839c
H-Yuuki <yuukirinnma@gmail.com>
2022-11-11 10:05:49 +0900

    add: 初期ファイル作成

---
+++ Flux/Flux/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flux
+//
+//  Created by 濵田　悠樹 on 2022/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+
+
+}
+
