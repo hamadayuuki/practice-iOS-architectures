050879808d8e7f64aa1c3fd755d568c7288ce701
hamadayuuki <yuukirinnma@gmail.com>
2023-03-23 19:43:28 +0900

    add: カウントを行うためのActionを作成

---
+++ TCA/TCA-counter/TCA-counter/Action/CounterAction.swift
@@ -0,0 +1,13 @@
+//
+//  CounterAction.swift
+//  TCA-counter
+//
+//  Created by 濵田　悠樹 on 2023/03/23.
+//
+
+import Foundation
+
+enum CounterAction: Equatable {
+    case decrementButtonTapped   // -
+    case incrementButtonTapped   // +
+}
