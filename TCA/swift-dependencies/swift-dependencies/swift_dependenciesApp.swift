8aa3b10a15c62aa4a91345e52ce4de31aeb1dff8
hamadayuuki <yuukirinnma@gmail.com>
2023-03-24 11:20:19 +0900

    add: swift-dependencies 初期ファイル作成

---
+++ TCA/swift-dependencies/swift-dependencies/swift_dependenciesApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_dependenciesApp.swift
+//  swift-dependencies
+//
+//  Created by 濵田　悠樹 on 2023/03/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_dependenciesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
