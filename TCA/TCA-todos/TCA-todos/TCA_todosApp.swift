9e75d636f37d650534cdb78a8baf6e16b1b690c1
hamadayuuki <yuukirinnma@gmail.com>
2023-03-29 12:12:15 +0900

    add: プロジェクト新規作成

---
+++ TCA/TCA-todos/TCA-todos/TCA_todosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_todosApp.swift
+//  TCA-todos
+//
+//  Created by 濵田　悠樹 on 2023/03/29.
+//
+
+import SwiftUI
+
+@main
+struct TCA_todosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
