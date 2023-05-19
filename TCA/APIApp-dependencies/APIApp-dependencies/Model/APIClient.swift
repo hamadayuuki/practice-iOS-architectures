1458a864d6642572d71d98aa481d9f49c25e3a5f
hamadayuuki <yuukirinnma@gmail.com>
2023-05-19 23:16:57 +0900

    add: register APIClient dependencies

--- TCA/APIApp-dependencies/APIApp-dependencies/Model/APIClient.swift
+++ TCA/APIApp-dependencies/APIApp-dependencies/Model/APIClient.swift
@@ -6,7 +6,9 @@
 //
 
 import Foundation
+import Dependencies
 
+// 1:DIするためにプロトコルとして宣言
 protocol APIClienttProtocol: Sendable {
     func fetch() async throws -> Data
 }
@@ -19,5 +21,21 @@ public struct APIClient: APIClienttProtocol {
     public func fetch() async throws -> Data {
         let (data, _) = try await URLSession.shared.data(from: url)
         return data
+    }
+}
+
+// 2:DependencyKey の登録
+private enum APIClientKey: DependencyKey {
+    // アプリ実行時使用
+    static let liveValue: any APIClienttProtocol = APIClient()
+}
+
+// 3:DependencyValues の拡張
+extension DependencyValues {
+    //  ↓ @Dependency で呼び出す時のKeyとなる
+    var apiClient: any APIClienttProtocol {
+        // 呼び出されるデータを getter, setter で管理
+        get { self[APIClientKey.self] }
+        set { self[APIClientKey.self] = newValue }
     }
 }
