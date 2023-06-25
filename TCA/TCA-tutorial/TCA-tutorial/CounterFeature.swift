ea69f2cf7e7c4e89cc11f5b26972be6d56706ee8
hamadayuuki <yuukirinnma@gmail.com>
2023-06-25 23:43:27 +0900

    add: State-Action-Reducer の骨組みを作成

---
+++ TCA/TCA-tutorial/TCA-tutorial/CounterFeature.swift
@@ -0,0 +1,45 @@
+//
+//  CounterFeature.swift
+//  TCA-tutorial
+//
+//  Created by 濵田　悠樹 on 2023/06/25.
+//
+
+import ComposableArchitecture
+
+// 1Feature = 1ReducerProtocol
+struct CounterFeature: ReducerProtocol {
+    struct State {
+        var count = 0
+    }
+    
+    /*
+     Tip
+         - It is best to name the Action cases after literally what the user does in the UI, such as incrementButtonTapped, rather than what logic you want to perform, such as incrementCount.
+         - Actionケースには、incrementCountのような実行したいロジックではなく、incrementButtonTappedのように、文字通りユーザーがUIで行うことにちなんだ名前をつけるのがベストだ。
+     */
+    enum Action {
+        case decrementButtonTapped
+        case incrementButtonTapped
+    }
+    
+    /*
+     Note
+        - The reduce method takes State as an argument and it is marked as inout. This means you can make any mutations you want directly to the state. There is no need to make a copy of the state just to return it.
+        - reduceメソッドはStateを引数にとり、inout(※)としてマークされる。つまり、ステートに直接好きな変異を加えることができる。ステートを返すためだけにそのコピーを作成する必要はない。
+     
+        ※ into, inout: 参照渡し, 関数の中でその引数に変更を加えた場合、呼び出し側の元の変数にも変更が適用される
+     */
+    func reduce(into state: inout State, action: Action) {
+        switch action {
+        case .decrementButtonTapped:
+            // decrementButton が押された時の処理
+            
+        case .incrementButtonTapped:
+            // incrementButton が押された時の処理
+            
+        }
+    }
+}
+
+
