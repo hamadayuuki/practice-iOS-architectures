2b5cebce65a2c254df9b517658fa913d6903cf06
hamadayuuki <yuukirinnma@gmail.com>
2023-06-26 00:18:23 +0900

    update: ContentView からCounterViewの呼び出しを可能に

--- TCA/TCA-tutorial/TCA-tutorial/ContentView.swift
+++ TCA/TCA-tutorial/TCA-tutorial/ContentView.swift
@@ -5,17 +5,27 @@
 //  Created by 濵田　悠樹 on 2023/06/25.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 struct ContentView: View {
+    let store = Store(initialState: CounterFeature.State(), reducer: {
+        CounterFeature()
+            ._printChanges()   // 変更を検知し、コンソールに表示
+        
+        /*
+         コンソール
+            ====
+            received action:
+               CounterFeature.Action.incrementButtonTapped
+             - CounterFeature.State(count: 0)
+             + CounterFeature.State(count: 1)
+            〜〜〜
+         */
+    })
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ConunterView(store: store)
     }
 }
 
