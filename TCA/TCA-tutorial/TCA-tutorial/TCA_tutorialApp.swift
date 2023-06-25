bacdd3cc58cf8b003cdd10a2a96af2a043976131
hamadayuuki <yuukirinnma@gmail.com>
2023-06-25 23:12:28 +0900

    add: init TCA-tutorial project

---
+++ TCA/TCA-tutorial/TCA-tutorial/TCA_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_tutorialApp.swift
+//  TCA-tutorial
+//
+//  Created by 濵田　悠樹 on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
